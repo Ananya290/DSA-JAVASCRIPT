2efb8fead31545cbd154dc35e767c2b82349770a
Ananya Shukla <annayashukla.ss290@gmail.com>
2025-06-21 16:17:09 +0530

    DSA-javascript

--- Basic-warmup/pallindrome/pallindrome.js
+++ Basic-warmup/pallindrome/pallindrome.js
@@ -1,3 +1,5 @@
+//Question 1 : Find the pallindrome number
+
 num = 121
 numCopy= num
 rev = 0;
@@ -12,3 +14,23 @@ if(numCopy == rev) {
 else{
       console.log("it  is not  a palindrome")
 }
+
+// By using function find the pallindrome.
+
+let number = 121
+copyNumber = number
+let rev = ""
+function reverseInteger(){
+ while(number>0){
+    rem = (number % 10)
+    rev += rem
+    number = Math.floor(number / 10)
+ }
+ if(copyNumber == rev){
+console.log("its a pallindrome")
+ }else{
+    console.log("it is not  a pallindrome")
+ }
+ return copyNumber == rev 
+}
+// console.log(reverseInteger())
---
+++ TimeAndSpaceComplexity/notes.js
@@ -0,0 +1,112 @@
+/*
+1.Time Complexity  --It is used to measure efficiency of algorithm in term of speed, as the input size grows.
+
+time complexity != time taken
+
+Speed Efficiency ---> When input size grows.
+
+
+Linear Search                        
+n = 10 , x = 10
+n = 100 , x = 100
+n = 1000 , x = 1000
+
+Binary Search
+n = 10 , x = 3
+n = 100 , x = 7
+n = 1000 , x = 10
+
+Binary Search Efficiency >>>> Linear Search Efficiency
+
+2. Big O notation ----Big O noattion represent worst case time complexity
+
+Linear Search 
+[5,6,1,0,7]
+Best Case - Search(5)
+Worst Case - Search(100)
+Linear search --> O(n)
+
+
+
+Binary Search 
+[5,6,7, 10 ,12]
+Best Case - Search(10)
+Worst Case - Search(100)
+Binary Search ---> O(log n)
+
+Efficiency 
+O(log n) >> O(n)
+
+
+O(n2) --- loop in loop , this one is more worst than O(n)
+O(n log n) ---- Merge Sort
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
